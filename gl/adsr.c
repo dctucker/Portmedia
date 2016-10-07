0a8c68f23800aba903751051ed55d3e1b87608d3
D. Casey Tucker <dcaseytucker@gmail.com>
2016-10-06 21:19:01 -0700

    sexy ADSR

--- gl/adsr.c
+++ gl/adsr.c
@@ -18,11 +18,14 @@ static const GLuint WIDTH = 1024, HEIGHT = 768;
 GLSL(vertexShaderSource,
 	layout (location = 0) in vec4 in_adsr;
 	layout (location = 1) in float in_vol;
+	layout (location = 2) in vec3 in_color;
 	out vec4 adsr;
 	out float vol;
+	out vec3 color;
 	void main() {
 		adsr = in_adsr;
 		vol  = in_vol;
+		color = in_color;
 	}
 );
 GLSL(geometryShaderSource,
@@ -32,41 +35,39 @@ GLSL(geometryShaderSource,
 
 	in vec4 adsr[];
 	in float vol[];
+	in vec3 color[];
 	out vec4 fragColor;
 
 	void main(){
-		float v =  max( 0.1, ( 40. + 20. * log( vol[0] )/log(10.0) ) * 0.025 ) ;
-		float a =  adsr[0].x;
-		float d =  adsr[0].y + a ;
-		float s =  adsr[0].z * v; //max( 0.0, ( 40. + 20. * log( adsr[0].z * vol[0] )/log(10.0) ) * 0.025 ) ;
-		float r =  adsr[0].w + d ;
+		float v =  clamp( (40. + 20. * log( vol[0] )/log(10.0) ) * 0.025, 0.1, 1.0 ) ;
+		float a =  clamp( adsr[0].x, 0, 1);
+		float d =  clamp( adsr[0].y + a, 0, 1);
+		float s =  clamp( adsr[0].z * v, 0, 1); //max( 0.0, ( 40. + 20. * log( adsr[0].z * vol[0] )/log(10.0) ) * 0.025 ) ;
+		float r =  clamp( adsr[0].w + d, 0, 1);
+		float r0=  clamp( adsr[0].w + d - 1, 0, 1);
 
 		float alpha = 0.4;
 		//if( selinst == i ) alpha = 0.6;
 
 		vec2 scaleVector = vec2(1.0, 1.0);
 
-		fragColor = vec4(0.0, 1.0, 1.0, alpha * 1.0 );
+		fragColor = vec4(color[0], alpha * 1.0 );
 		gl_Position = vec4( scaleVector * vec2( 0, 0 ), 1.0, 1.0); EmitVertex();
 		gl_Position = vec4( scaleVector * vec2( a, 0 ), 1.0, 1.0); EmitVertex();
 		gl_Position = vec4( scaleVector * vec2( a, v ), 1.0, 1.0); EmitVertex();
-		EndPrimitive();
-		
-		fragColor = vec4(0.0, 1.0, 1.0, alpha * 0.9 );
-		gl_Position = vec4( scaleVector * vec2( a, v ), 1.0, 1.0); EmitVertex();
+		fragColor = vec4(color[0], alpha * 0.7 );
+		//gl_Position = vec4( scaleVector * vec2( a, v ), 1.0, 1.0); EmitVertex();
 		gl_Position = vec4( scaleVector * vec2( d, s ), 1.0, 1.0); EmitVertex();
 		gl_Position = vec4( scaleVector * vec2( a, 0 ), 1.0, 1.0); EmitVertex();
-		EndPrimitive();
-		
-		fragColor = vec4(0.0, 1.0, 1.0, alpha * 1.0 );
-		gl_Position = vec4( scaleVector * vec2( a, 0 ), 1.0, 1.0); EmitVertex();
+		fragColor = vec4(color[0], alpha * 0.9 );
+		//gl_Position = vec4( scaleVector * vec2( a, 0 ), 1.0, 1.0); EmitVertex();
 		gl_Position = vec4( scaleVector * vec2( d, 0 ), 1.0, 1.0); EmitVertex();
+		fragColor = vec4(color[0], alpha * 0.9 );
 		gl_Position = vec4( scaleVector * vec2( d, s ), 1.0, 1.0); EmitVertex();
-		EndPrimitive();
-
-		fragColor = vec4(0.0, 1.0, 1.0, alpha * 0.9 );
-		gl_Position = vec4( scaleVector * vec2( d, s ), 1.0, 1.0); EmitVertex();
+		//gl_Position = vec4( scaleVector * vec2( d, s ), 1.0, 1.0); EmitVertex();
 		gl_Position = vec4( scaleVector * vec2( d, 0 ), 1.0, 1.0); EmitVertex();
+		fragColor = vec4(color[0], alpha * 0.5 );
+		gl_Position = vec4( scaleVector * vec2( r, r0 ), 1.0, 1.0); EmitVertex();
 		gl_Position = vec4( scaleVector * vec2( r, 0 ), 1.0, 1.0); EmitVertex();
 		EndPrimitive();
 	}
@@ -79,11 +80,9 @@ GLSL(fragmentShaderSource,
 	}
 );
 GLfloat adsr[] = {
-	0.1,
-	0.2,
-	1.0,
-	0.2,
-	0.0
+//   A     D    S    R    V    R    G    B
+	0.10, 0.2, 1.0, 0.5, 0.5, 1.0, 0.0, 0.0,
+	0.05, 0.1, 0.7, 0.1, 0.1, 0.0, 1.0, 1.0,
 };
 
 #define SETUP_SHADER(type, shader, shadersource) GLint shader = glCreateShader(type); { \
@@ -145,11 +144,14 @@ int main(void) {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(adsr), adsr, GL_STATIC_DRAW);
 	/* adsr */
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	/* volume */
-	glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(4 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(4 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	/* color */
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(5 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(0);
 
